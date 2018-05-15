31cb03ba740120abeebcede7ac270ea53295883f
minghaozhi <597575122@qq.com>
2018-05-15 14:32:01 +0800

    eureka服务提供者1

---
+++ eureka-client/src/main/java/com/eurekaClient/EurekaClientApplication.java
@@ -0,0 +1,18 @@
+package com.eurekaClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author 墨殇
+ * @description:
+ * @Date : 2018/5/15.
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
