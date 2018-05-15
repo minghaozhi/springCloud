28f8562beaa45bc506ad3ebc7853fdf1b2e63ec5
minghaozhi <597575122@qq.com>
2018-05-15 14:31:15 +0800

    eureka服务注册中心

---
+++ erueka-server/src/main/java/com/eurekaServer/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 墨殇
+ * @description:
+ * @Date : 2018/5/15.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
